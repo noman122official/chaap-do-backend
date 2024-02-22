a69df32dbe70540cb47f937d28fd3d2bfd91fad7
Naman Das <namandas.123@gmail.com>
2024-02-23 00:15:54 +0530

    First Commit - Initializing Express and Mongoose

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
